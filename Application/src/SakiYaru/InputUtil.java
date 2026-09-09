f1b3bb6cf7c0c7f14221aa03588cc70316fc39b9
Daichi0321 <nagaminedaichi0321@gmail.com>
2026-09-09 14:06:32 +0900

    メニュー選択までのInputUtil.javaを実装

---
+++ Application/src/SakiYaru/InputUtil.java
@@ -0,0 +1,29 @@
+//入力受付・入力チェック
+package SakiYaru;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputUtil {
+	Scanner scan = new Scanner(System.in);
+
+	public int inputInt() {
+		int menuNumber;
+		while (true) {
+			try {
+				menuNumber = scan.nextInt();
+			} catch (InputMismatchException inputMiss) {
+				System.out.println("入力した値は整数ではありません");
+				System.out.println("数字を再入力してください");
+				scan.nextLine();
+				continue;
+			}
+			if (menuNumber >= 0 && menuNumber <= 5) {
+				return menuNumber;
+			} else {
+				System.out.println("入力した値は0~5ではありません");
+				System.out.println("数字を再入力してください");
+			}
+		}
+	}
+}
